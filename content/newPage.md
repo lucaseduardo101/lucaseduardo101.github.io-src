2bb6ed63b148867af90af8eff3d9522ba4b27b5a
unknown <lucaseduardo101@gmail.com>
2020-05-03 16:03:54 -0300

    testando novas páginas

---
+++ content/newPage.md
@@ -0,0 +1,5 @@
+Title: New Page test
+Date: 2020-05-03 11:01
+Category: Pelican
+
+I want learn how use Pelican to create pages
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-THEME = 'pelican-blueidea'
+#THEME = 'pelican-blueidea'
 
 AUTHOR = 'Lucas Oliveira'
 SITENAME = 'lucaseduardo101'
